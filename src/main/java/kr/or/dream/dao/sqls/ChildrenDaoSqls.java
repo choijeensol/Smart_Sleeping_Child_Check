815d2036ba5be52c742ffb84928d3c18bca1b44c
Minki Park <75825682+JGatsby-29@users.noreply.github.com>
2021-07-18 19:41:35 +0900

    Delete src/main directory

--- src/main/java/kr/or/dream/dao/sqls/ChildrenDaoSqls.java
+++
@@ -1,8 +0,0 @@
-package kr.or.dream.dao.sqls;
-
-public class ChildrenDaoSqls {
-	public static final String SELECT_CHILDREN_ALL = "SELECT * FROM children";
-	public static final String SELECT_CHILDREN = "SELECT * FROM children WHERE no=:no";
-	public static final String UPDATE_CHILDREN = "UPDATE children SET name=:name, age=:age, class_no=:classNo WHERE no=:no";
-	public static final String DELETE_CHILDREN = "DELETE FROM children WHERE no=:no";
-}
